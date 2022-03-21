0d3f5089392979fb6f75b65872e4ff9e904ddf83
Daren <cowardswithbadges.com>
2022-03-21 01:01:56 -0400

    update

---
+++ internal/pages/about.svelte-37b9308d.js
@@ -0,0 +1,65 @@
+import{S as Je,i as Ne,s as Le,k as d,e as i,t as s,_ as Ke,d as a,m as h,c as o,a as r,h as l,b as c,g as De,J as e,K as _e}from"../chunks/vendor-ead1d6ea.js";import{d as Re,b as Ve}from"../chunks/env-859c48a7.js";import{b as I}from"../chunks/paths-4b3c6e7e.js";function Fe(Ie){let _,t,T,J,N,O,L,K,R,V,y,f,j,F,G,S,M,Q,z,U,X,C,Y,Z,b,m,W,$,ee,u,te,x,ne,ae,E,ie,oe,P,re,se,le,H,ce,de,q,he,ue,w,p,B,fe,me,k,pe,A,ge,ve;return{c(){_=d(),t=i("div"),T=i("h1"),J=s("about our cause"),N=d(),O=i("p"),L=s(`Our vision is to ensure a society where law enforcement no longer serves a racist system that
+		bases itself on lies, discrimination, and has a finanical agenda. We support divesting funds
+		from police departments and reallocating to non-policing forms of mental health care, community
+		support, education, employment, and other community resources.`),K=d(),R=i("hr"),V=d(),y=i("h4"),f=i("a"),j=i("span"),F=s("the system is corrupt"),G=d(),S=i("p"),M=s(`The current judicial process in America is designed for individuals that have been arrested of a
+		crime, either innocent or guilty - to fail. This system's focal point is to generate money for
+		government. The victims of this system lose their children, jobs, homes, finanical independence,
+		and their lives.`),Q=d(),z=i("p"),U=s(`While there are many reasons for an individual to break a law, the jail and prison systems offer
+		no education, training, or support to help a person turn their live around. The criminal courts
+		and probation services force individuals to pay an unfair amount of fees ranging from court
+		costs, restitution, child support, probation, and many other avenues that would be considered
+		extortion.`),X=d(),C=i("p"),Y=s(`If these fees are not paid on time, often the individuals will lose their driver's
+		license. Over time this leads to unemployment, as criminal backgrounds drastically reduces
+		employment opportunities. By design, all these facets causes individuals caught in this sytem to
+		commit more crimes and continue the cycle of government making money off of them.`),Z=d(),b=i("h4"),m=i("a"),W=i("span"),$=s("Police Officers are under-trained & over-armed"),ee=d(),u=i("p"),te=s(`Police Officers have demonstrated time and time again that they are under-trained and
+		over-armed. These officers `),x=i("a"),ne=s("do not know the laws"),ae=s(" they are enforcing. "),E=i("a"),ie=s("Deallocating funds that militarize law enforcement"),oe=s(` need to be put to use into a fund that supports an individual's
+		education, employment, mental health, and family support. Police Officers can often be observed
+		on a daily basis looking for petty crimes, for example: a broken tail light, jay-walking,
+		running a stop sign, disorderly conduct, victimless crimes such as a person in possession of
+		marijuana. Law enforcement officers use these crimes as a way to look or fish for more
+		infractions. Police Officers lie, make up laws, and `),P=i("a"),re=s("violate our rights"),se=s(" every day."),le=d(),H=i("p"),ce=s(`As a society, we've been trained to believe that laws against petty crimes is for public saftey,
+		however that is far from the truth. The term known as "broken windows policing", meaning
+		targeting low-level offenses, defunding those areas of police work and allocating those funds to
+		another agency that provides replacement vehicle lights, anger management courses, mental health
+		evaluations and medications, and training on changing habits would stop the financial incentive
+		for policing and government. Society would be encouraged to use those resources instead of law
+		enforcement using overcriminalization, violating citizen's rights and excessive use of force.`),de=d(),q=i("p"),he=s(`Society would be encouraged to use those resources instead of law
+		enforcement using overcriminalization, violating our rights, excessive use of force, and killing
+		people.`),ue=d(),w=i("h4"),p=i("a"),B=i("span"),fe=s("providing education and exposing corruption"),me=d(),k=i("p"),pe=s(`Corrupt Police Officers, Judges, Attorneys, Child Protective Agents, courts, and other
+		government officials such as commissioners and mayors, will use their badge and authority to
+		commit crimes but cannot be held accountable due to qualified immunity. Their crimes are often
+		extortion, theft, planting/destorying evidence, civil rights violations, racism, kidnapping,
+		rape, sexual assault on children, human trafficking, and murder. They are `),A=i("a"),ge=s("Cowards with Badges"),ve=s(`. We will expose, educate, and document these cowards and break their immunity safe
+		haven.`),this.h()},l(g){Ke('[data-svelte="svelte-1ine71f"]',document.head).forEach(a),_=h(g),t=o(g,"DIV",{class:!0});var n=r(t);T=o(n,"H1",{});var xe=r(T);J=l(xe,"about our cause"),xe.forEach(a),N=h(n),O=o(n,"P",{});var Ee=r(O);L=l(Ee,`Our vision is to ensure a society where law enforcement no longer serves a racist system that
+		bases itself on lies, discrimination, and has a finanical agenda. We support divesting funds
+		from police departments and reallocating to non-policing forms of mental health care, community
+		support, education, employment, and other community resources.`),Ee.forEach(a),K=h(n),R=o(n,"HR",{}),V=h(n),y=o(n,"H4",{id:!0});var be=r(y);f=o(be,"A",{"aria-hidden":!0,tabindex:!0,href:!0});var Pe=r(f);j=o(Pe,"SPAN",{class:!0}),r(j).forEach(a),Pe.forEach(a),F=l(be,"the system is corrupt"),be.forEach(a),G=h(n),S=o(n,"P",{});var Ae=r(S);M=l(Ae,`The current judicial process in America is designed for individuals that have been arrested of a
+		crime, either innocent or guilty - to fail. This system's focal point is to generate money for
+		government. The victims of this system lose their children, jobs, homes, finanical independence,
+		and their lives.`),Ae.forEach(a),Q=h(n),z=o(n,"P",{});var Te=r(z);U=l(Te,`While there are many reasons for an individual to break a law, the jail and prison systems offer
+		no education, training, or support to help a person turn their live around. The criminal courts
+		and probation services force individuals to pay an unfair amount of fees ranging from court
+		costs, restitution, child support, probation, and many other avenues that would be considered
+		extortion.`),Te.forEach(a),X=h(n),C=o(n,"P",{});var Oe=r(C);Y=l(Oe,`If these fees are not paid on time, often the individuals will lose their driver's
+		license. Over time this leads to unemployment, as criminal backgrounds drastically reduces
+		employment opportunities. By design, all these facets causes individuals caught in this sytem to
+		commit more crimes and continue the cycle of government making money off of them.`),Oe.forEach(a),Z=h(n),b=o(n,"H4",{id:!0});var we=r(b);m=o(we,"A",{"aria-hidden":!0,tabindex:!0,href:!0});var je=r(m);W=o(je,"SPAN",{class:!0}),r(W).forEach(a),je.forEach(a),$=l(we,"Police Officers are under-trained & over-armed"),we.forEach(a),ee=h(n),u=o(n,"P",{});var v=r(u);te=l(v,`Police Officers have demonstrated time and time again that they are under-trained and
+		over-armed. These officers `),x=o(v,"A",{href:!0});var Se=r(x);ne=l(Se,"do not know the laws"),Se.forEach(a),ae=l(v," they are enforcing. "),E=o(v,"A",{href:!0});var ze=r(E);ie=l(ze,"Deallocating funds that militarize law enforcement"),ze.forEach(a),oe=l(v,` need to be put to use into a fund that supports an individual's
+		education, employment, mental health, and family support. Police Officers can often be observed
+		on a daily basis looking for petty crimes, for example: a broken tail light, jay-walking,
+		running a stop sign, disorderly conduct, victimless crimes such as a person in possession of
+		marijuana. Law enforcement officers use these crimes as a way to look or fish for more
+		infractions. Police Officers lie, make up laws, and `),P=o(v,"A",{href:!0});var Ce=r(P);re=l(Ce,"violate our rights"),Ce.forEach(a),se=l(v," every day."),v.forEach(a),le=h(n),H=o(n,"P",{});var We=r(H);ce=l(We,`As a society, we've been trained to believe that laws against petty crimes is for public saftey,
+		however that is far from the truth. The term known as "broken windows policing", meaning
+		targeting low-level offenses, defunding those areas of police work and allocating those funds to
+		another agency that provides replacement vehicle lights, anger management courses, mental health
+		evaluations and medications, and training on changing habits would stop the financial incentive
+		for policing and government. Society would be encouraged to use those resources instead of law
+		enforcement using overcriminalization, violating citizen's rights and excessive use of force.`),We.forEach(a),de=h(n),q=o(n,"P",{});var He=r(q);he=l(He,`Society would be encouraged to use those resources instead of law
+		enforcement using overcriminalization, violating our rights, excessive use of force, and killing
+		people.`),He.forEach(a),ue=h(n),w=o(n,"H4",{id:!0});var ke=r(w);p=o(ke,"A",{"aria-hidden":!0,tabindex:!0,href:!0});var qe=r(p);B=o(qe,"SPAN",{class:!0}),r(B).forEach(a),qe.forEach(a),fe=l(ke,"providing education and exposing corruption"),ke.forEach(a),me=h(n),k=o(n,"P",{});var D=r(k);pe=l(D,`Corrupt Police Officers, Judges, Attorneys, Child Protective Agents, courts, and other
+		government officials such as commissioners and mayors, will use their badge and authority to
+		commit crimes but cannot be held accountable due to qualified immunity. Their crimes are often
+		extortion, theft, planting/destorying evidence, civil rights violations, racism, kidnapping,
+		rape, sexual assault on children, human trafficking, and murder. They are `),A=o(D,"A",{href:!0});var Be=r(A);ge=l(Be,"Cowards with Badges"),Be.forEach(a),ve=l(D,`. We will expose, educate, and document these cowards and break their immunity safe
+		haven.`),D.forEach(a),n.forEach(a),this.h()},h(){document.title="About",c(j,"class","icon icon-link"),c(f,"aria-hidden","true"),c(f,"tabindex","-1"),c(f,"href","#the-system-is-corrupt"),c(y,"id","the-system-is-corrupt"),c(W,"class","icon icon-link"),c(m,"aria-hidden","true"),c(m,"tabindex","-1"),c(m,"href","#police-officers-are-under-trained"),c(b,"id","police-officers-are-under-trained"),c(x,"href",I+"/resources/officers-donot-have-to-know-law"),c(E,"href",I+"/resources/defunding-police"),c(P,"href",I+"/resources/categories/human-rights"),c(B,"class","icon icon-link"),c(p,"aria-hidden","true"),c(p,"tabindex","-1"),c(p,"href","#providing-education"),c(w,"id","providing-education"),c(A,"href",I+"/"),c(t,"class","content")},m(g,ye){De(g,_,ye),De(g,t,ye),e(t,T),e(T,J),e(t,N),e(t,O),e(O,L),e(t,K),e(t,R),e(t,V),e(t,y),e(y,f),e(f,j),e(y,F),e(t,G),e(t,S),e(S,M),e(t,Q),e(t,z),e(z,U),e(t,X),e(t,C),e(C,Y),e(t,Z),e(t,b),e(b,m),e(m,W),e(b,$),e(t,ee),e(t,u),e(u,te),e(u,x),e(x,ne),e(u,ae),e(u,E),e(E,ie),e(u,oe),e(u,P),e(P,re),e(u,se),e(t,le),e(t,H),e(H,ce),e(t,de),e(t,q),e(q,he),e(t,ue),e(t,w),e(w,p),e(p,B),e(w,fe),e(t,me),e(t,k),e(k,pe),e(k,A),e(A,ge),e(k,ve)},p:_e,i:_e,o:_e,d(g){g&&a(_),g&&a(t)}}}const Ue=Re,Xe=Ve,Ye=!0;class Ze extends Je{constructor(_){super();Ne(this,_,null,Fe,Le,{})}}export{Ze as default,Ue as hydrate,Ye as prerender,Xe as router};
